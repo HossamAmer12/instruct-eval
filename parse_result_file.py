4721c27256c402485428d1df8cbeb0786a1e19ec
Hossam Amer <hossam.amer12@gmail.com>
2025-04-10 23:22:38 +0000

    extract step and training tokens from fname

--- parse_result_file.py
+++ parse_result_file.py
@@ -1,6 +1,20 @@
 import sys
 import os
 import json
+import re
+
+def extract_fname_info(fname):
+    # Extract step and training size (e.g., "480k", "1T")
+    match = re.search(r"step-(\w+)-(\w+)_snapshots", fname)
+    step = -1
+    training_size= -1
+    if match:
+        step = match.group(1)
+        training_size = match.group(2)
+        # print(f"Step: {step}, Training size: {training_size}")
+    else:
+        print("Pattern not found.")
+    return step, training_size
 
 def parse_file(fname):
     with open(fname) as f:
@@ -20,3 +34,7 @@ fname = "humaneval__data00_maryam_models--TinyLlama--TinyLlama-1.1B-intermediate
 pass_results = parse_file(fname=fname)
 
 print(pass_results)
+step, training_tokens = extract_fname_info(fname=fname)
+
+print(step, training_tokens)
+
