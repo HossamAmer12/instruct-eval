8070332767127f86766f85ac416a70f9c392f75a
Hossam Amer <hossam.amer12@gmail.com>
2025-04-12 18:38:55 +0000

    baseline server110 running tinnyllama

--- baseline_server110_ttc_run.sh
+++ baseline_server110_ttc_run.sh
@@ -26,17 +26,15 @@
 # )
 
 MODEL_PATHS=(
-    "/data00/dataset/tyllama//step-5k-token-10B/models--TinyLlama--tinyLlama-intermediate-checkpoints/snapshots/feb60e43f33c2b8db5b122dbaef76b9dffd557c8"
-    "/data00/dataset/tyllama//step-65k-token-136B/models--TinyLlama--tinyLlama-intermediate-checkpoints/snapshots/b68428d743c49097f905b9e31d0d27098e3cd44f"
-    "/data00/dataset/tyllama//step-125k-token-262B/models--TinyLlama--tinyLlama-intermediate-checkpoints/snapshots/c4d3599fc7894e4e9e34154e1a84d165fcff016a"
-    "/data00/dataset/tyllama//step-185k-token-388B/models--TinyLlama--tinyLlama-intermediate-checkpoints/snapshots/e58f49895fdf004fa8d0e5a57d42e17c8867b12c"
-    "/data00/dataset/tyllama//step-240k-token-503B/models--TinyLlama--tinyLlama-intermediate-checkpoints/snapshots/cab66f52ac47ad5fc57e9510485c69c02cb99d09"
-    "/data00/dataset/tyllama//step-300k-token-629B/models--TinyLlama--tinyLlama-intermediate-checkpoints/snapshots/5f9d394fc605f8027f88682e563b104c623fe4ed"
-    "/data00/dataset/tyllama//step-360k-token-755B/models--TinyLlama--tinyLlama-intermediate-checkpoints/snapshots/223a4964750d14dd0265ce9d5a56b4f1c540b15b"
-    "/data00/dataset/tyllama//step-420k-token-881B/models--TinyLlama--tinyLlama-intermediate-checkpoints/snapshots/87ac1fb103c323bafb34dc42b97a30aa4de225e1"
-    "/data00/dataset/tyllama//step-480k-token-1007B/models--TinyLlama--tinyLlama-intermediate-checkpoints/snapshots/68c667a2869968a1d527edd85b578e54696434b2"
-    "/data00/dataset/tyllama//step-600k-token-1258B/models--TinyLlama--tinyLlama-intermediate-checkpoints/snapshots/f056976086771d075133feda7bad88047c1b7802"
-    "/data00/dataset/tyllama//step-660k-token-1384B/models--TinyLlama--tinyLlama-intermediate-checkpoints/snapshots/91804715ca0e86a0bf67465a52383fe955462e54"
+        "/data00/dataset/tinyllama/step-5k-token-10B/models--TinyLlama--tinyLlama-intermediate-checkpoints/snapshots/feb60e43f33c2b8db5b122dbaef76b9dffd557c8"
+        "/data00/dataset/tinyllama/step-65k-token-136B/models--TinyLlama--tinyLlama-intermediate-checkpoints/snapshots/b68428d743c49097f905b9e31d0d27098e3cd44f"
+        "/data00/dataset/tinyllama/step-125k-token-262B/models--TinyLlama--tinyLlama-intermediate-checkpoints/snapshots/c4d3599fc7894e4e9e34154e1a84d165fcff016a"
+        "/data00/dataset/tinyllama/step-185k-token-388B/models--TinyLlama--tinyLlama-intermediate-checkpoints/snapshots/e58f49895fdf004fa8d0e5a57d42e17c8867b12c"
+        "/data00/dataset/tinyllama/step-240k-token-503B/models--TinyLlama--tinyLlama-intermediate-checkpoints/snapshots/cab66f52ac47ad5fc57e9510485c69c02cb99d09"
+        "/data00/dataset/tinyllama/step-300k-token-629B/models--TinyLlama--tinyLlama-intermediate-checkpoints/snapshots/5f9d394fc605f8027f88682e563b104c623fe4ed"
+        "/data00/dataset/tinyllama/step-360k-token-755B/models--TinyLlama--tinyLlama-intermediate-checkpoints/snapshots/223a4964750d14dd0265ce9d5a56b4f1c540b15b"
+        "/data00/dataset/tinyllama/step-420k-token-881B/models--TinyLlama--tinyLlama-intermediate-checkpoints/snapshots/87ac1fb103c323bafb34dc42b97a30aa4de225e1"
+        "/data00/dataset/tinyllama/step-660k-token-1384B/models--TinyLlama--tinyLlama-intermediate-checkpoints/snapshots/91804715ca0e86a0bf67465a52383fe955462e54"
 )
 
 
